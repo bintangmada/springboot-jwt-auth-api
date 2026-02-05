3ed6cf8ca613a5e3d0ebf01b63061c3beaebced9
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-05 15:17:28 +0700

    chore: initialize Spring Boot project

---
+++ src/test/java/com/bintang/jwt/auth/SpringbootJwtAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bintang.jwt.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
