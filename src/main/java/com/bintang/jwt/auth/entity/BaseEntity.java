fb25f03bb803c2f35af7c8e5ae7d2592df729c40
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-06 10:22:31 +0700

    feat: add base entity

---
+++ src/main/java/com/bintang/jwt/auth/entity/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.bintang.jwt.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+    private String deletedBy;
+
+    @Column(nullable = false)
+    private Long status;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+}
