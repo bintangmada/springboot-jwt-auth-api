93c1de3f71d3a9129a8231337438ae419f496ffb
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-09 14:56:13 +0700

    feat: add login controller

--- src/main/java/com/bintang/jwt/auth/controller/AuthController.java
+++ src/main/java/com/bintang/jwt/auth/controller/AuthController.java
@@ -1,2 +1,32 @@
-package com.bintang.jwt.auth.controller;public class AuthController {
+package com.bintang.jwt.auth.controller;
+
+import com.bintang.jwt.auth.dto.auth.LoginRequest;
+import com.bintang.jwt.auth.security.jwt.JwtUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public String login(@RequestBody @Valid LoginRequest request) {
+
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+        return jwtUtil.generateToken((UserDetails) auth.getPrincipal());
+    }
 }
