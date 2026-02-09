bc39758d0dfc70a2e4daaf747e7badc918a30103
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-09 10:14:47 +0700

    feat: add OAuth2SuccessHandler to filter chain

--- src/main/java/com/bintang/jwt/auth/config/SecurityConfig.java
+++ src/main/java/com/bintang/jwt/auth/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.bintang.jwt.auth.config;
 
 import com.bintang.jwt.auth.security.jwt.JwtAuthenticationFilter;
+import com.bintang.jwt.auth.security.oauth2.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtFilter;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -24,6 +26,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login(oauth -> oauth
+                        .successHandler(oAuth2SuccessHandler))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/oauth2/**").permitAll()
                         .anyRequest().authenticated()
