61f26e3b22bcc56173fbaab2dceb196aa207c785
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-09 15:10:02 +0700

    feat: add Auth Service

---
+++ src/main/java/com/bintang/jwt/auth/service/AuthService.java
@@ -0,0 +1,40 @@
+package com.bintang.jwt.auth.service;
+
+import com.bintang.jwt.auth.dto.auth.AuthResponse;
+import com.bintang.jwt.auth.dto.auth.LoginRequest;
+import com.bintang.jwt.auth.repository.UserRepository;
+import com.bintang.jwt.auth.security.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthResponse login(LoginRequest request) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String token = jwtUtil.generateToken(userDetails);
+
+        return new AuthResponse(token);
+
+    }
+
+}
