686f7f603e5a2371bf8e6f7a1b434f365d92aeaa
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-09 16:55:35 +0700

    feat: add register controller

--- src/main/java/com/bintang/jwt/auth/controller/AuthController.java
+++ src/main/java/com/bintang/jwt/auth/controller/AuthController.java
@@ -2,10 +2,12 @@ package com.bintang.jwt.auth.controller;
 
 import com.bintang.jwt.auth.dto.auth.AuthResponse;
 import com.bintang.jwt.auth.dto.auth.LoginRequest;
+import com.bintang.jwt.auth.dto.user.RegisterRequest;
 import com.bintang.jwt.auth.security.jwt.JwtUtil;
 import com.bintang.jwt.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,5 +28,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<V oid> register(@RequestBody @Valid RegisterRequest request){
+        authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
