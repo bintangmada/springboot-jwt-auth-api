0f0f9f385498effc4684507326261653490fe00e
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-11 16:37:26 +0700

    feat: add role repository

--- src/main/java/com/bintang/jwt/auth/repository/RoleRepository.java
+++ src/main/java/com/bintang/jwt/auth/repository/RoleRepository.java
@@ -4,4 +4,7 @@ import com.bintang.jwt.auth.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    boolean existsByName(String name);
+
 }
