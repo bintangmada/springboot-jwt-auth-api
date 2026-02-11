0906ec111473594af5710963f7d4c52b4b38e06a
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-11 11:23:07 +0700

    feat: add find permission names by user id

--- src/main/java/com/bintang/jwt/auth/repository/mapping/RolePermissionRepository.java
+++ src/main/java/com/bintang/jwt/auth/repository/mapping/RolePermissionRepository.java
@@ -2,6 +2,8 @@ package com.bintang.jwt.auth.repository.mapping;
 
 import com.bintang.jwt.auth.entity.RolePermission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,7 @@ public interface RolePermissionRepository extends JpaRepository<RolePermission, 
     List<RolePermission> findByRoleId(Long roleId);
     void deleteByRoleIdAndPermissionId(Long roleId, Long permissionId);
     boolean existsByRoleIdAndPermissionId(Long roleId, Long permissionId);
+
+    @Query(value = "SELECT DISTINCT p.name FROM permissions p JOIN role_permission rp ON rp.permission_id = p.id JOIN user_roles ur ON ur.role_id = rp_role_id WHERE ur_user_id = :userId", nativeQuery = true)
+    List<String> findPermissionNamesByUserId(@Param("userId") Long userId);
 }
