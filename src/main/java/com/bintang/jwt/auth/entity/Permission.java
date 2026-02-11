45c3a7033ee060f3848b8ea3fee43aeb585b9e77
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-11 09:48:21 +0700

    feat: update permission entity

--- src/main/java/com/bintang/jwt/auth/entity/Permission.java
+++ src/main/java/com/bintang/jwt/auth/entity/Permission.java
@@ -1,21 +1,25 @@
 package com.bintang.jwt.auth.entity;
 
+import com.bintang.jwt.auth.entity.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "permissions")
+@Where(clause = "is_deleted = false")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Permission {
+public class Permission extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String name;
 
 }
