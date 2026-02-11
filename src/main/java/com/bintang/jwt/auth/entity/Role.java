4b7335d4d84761320d93a735081cf41840cf0ca9
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-11 09:46:00 +0700

    feat: update Role entity

--- src/main/java/com/bintang/jwt/auth/entity/Role.java
+++ src/main/java/com/bintang/jwt/auth/entity/Role.java
@@ -1,21 +1,27 @@
 package com.bintang.jwt.auth.entity;
 
+import com.bintang.jwt.auth.entity.base.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "roles")
+@Where(clause = "is_deleted = false")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Role {
+@SuperBuilder
+public class Role extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String name;
 
 }
