4222f2c53a1a83be05dc2fb8fa3fb369c5cb4972
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-11 11:14:33 +0700

    feat: add find role names by user id

--- src/main/java/com/bintang/jwt/auth/repository/mapping/UserRoleRepository.java
+++ src/main/java/com/bintang/jwt/auth/repository/mapping/UserRoleRepository.java
@@ -2,13 +2,20 @@ package com.bintang.jwt.auth.repository.mapping;
 
 import com.bintang.jwt.auth.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
     List<UserRole> findByUserId(Long userId);
+
     void deleteByUserIdAndRoleId(Long userId, Long roleId);
+
     boolean existsByUserIdAndRoleId(Long userId, Long roleId);
+
+    @Query(value = "SELECT r.name FROM roles r JOIN user_roles ur ON ur.role_id = r.id WHERE ur.user_id = :userId", nativeQuery = true)
+    List<String> findRoleNamesByUserId(@Param("userId") Long userId);
 
 }
