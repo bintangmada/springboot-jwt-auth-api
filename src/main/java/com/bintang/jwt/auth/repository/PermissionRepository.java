0553cf8b2f797031799b2d18d134b38d699b0e15
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-12 14:36:14 +0700

    feat: add exists by name

--- src/main/java/com/bintang/jwt/auth/repository/PermissionRepository.java
+++ src/main/java/com/bintang/jwt/auth/repository/PermissionRepository.java
@@ -4,4 +4,7 @@ import com.bintang.jwt.auth.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
+
+    boolean existsByName(String name);
+
 }
