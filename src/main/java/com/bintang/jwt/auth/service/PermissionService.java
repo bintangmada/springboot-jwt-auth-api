705ac659c6a19b6fdb5ce048b2732c3eede0b05f
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-12 14:36:43 +0700

    feat: add create permission service

--- src/main/java/com/bintang/jwt/auth/service/PermissionService.java
+++ src/main/java/com/bintang/jwt/auth/service/PermissionService.java
@@ -1,5 +1,8 @@
 package com.bintang.jwt.auth.service;
 
+import com.bintang.jwt.auth.dto.permission.PermissionRequest;
+import com.bintang.jwt.auth.dto.permission.PermissionResponse;
+import com.bintang.jwt.auth.entity.Permission;
 import com.bintang.jwt.auth.repository.PermissionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,21 @@ public class PermissionService {
 
     private final PermissionRepository permissionRepository;
 
+    public PermissionResponse create (PermissionRequest request){
+        if(permissionRepository.existsByName(request.getName())){
+            throw new RuntimeException("Permission already exists");
+        }
 
+        Permission permission = new Permission();
+        permission.setName(request.getName());
+        return mapToResponse(permissionRepository.save(permission));
+    }
+
+    PermissionResponse mapToResponse(Permission permission){
+        return PermissionResponse.builder()
+                .id(permission.getId())
+                .name(permission.getName())
+                .build();
+    }
 
 }
