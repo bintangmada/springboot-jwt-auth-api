137bd4bd87208d6648af33ea256057803885d065
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-12 14:26:43 +0700

    refactor: moving dto

--- src/main/java/com/bintang/jwt/auth/dto/permission/PermissionRequest.java
+++ src/main/java/com/bintang/jwt/auth/dto/permission/PermissionRequest.java
@@ -1,4 +1,4 @@
-package com.bintang.jwt.auth.dto;
+package com.bintang.jwt.auth.dto.permission;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
---
+++ src/main/java/com/bintang/jwt/auth/dto/permission/PermissionResponse.java
@@ -0,0 +1,2 @@
+package com.bintang.jwt.auth.dto.permission;public class PermissionResponse {
+}
