0b8c771c363b16c4894599b3a377f421a4f84537
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-13 14:00:00 +0700

    feat: add update permission

--- src/main/java/com/bintang/jwt/auth/controller/PermissionController.java
+++ src/main/java/com/bintang/jwt/auth/controller/PermissionController.java
@@ -2,16 +2,14 @@ package com.bintang.jwt.auth.controller;
 
 import com.bintang.jwt.auth.dto.permission.PermissionRequest;
 import com.bintang.jwt.auth.dto.permission.PermissionResponse;
+import com.bintang.jwt.auth.entity.Permission;
 import com.bintang.jwt.auth.repository.PermissionRepository;
 import com.bintang.jwt.auth.service.PermissionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/permissions")
@@ -21,7 +19,13 @@ public class PermissionController {
     private final PermissionService permissionService;
 
     @PostMapping
-    public ResponseEntity<PermissionResponse> create(@RequestBody @Valid PermissionRequest request){
+    public ResponseEntity<PermissionResponse> create(@RequestBody @Valid PermissionRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(permissionService.create(request));
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PermissionResponse> update(@PathVariable("id") Long id, @RequestBody @Valid PermissionRequest request) {
+        return ResponseEntity.ok().body(permissionService.update(id, request));
+    }
+
 }
