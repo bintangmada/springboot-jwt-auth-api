de57714c1fd4267cc3756a8e75f9010265da0c0e
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-13 13:48:07 +0700

    feat: add createpermission

--- src/main/java/com/bintang/jwt/auth/controller/PermissionController.java
+++ src/main/java/com/bintang/jwt/auth/controller/PermissionController.java
@@ -1,8 +1,15 @@
 package com.bintang.jwt.auth.controller;
 
+import com.bintang.jwt.auth.dto.permission.PermissionRequest;
+import com.bintang.jwt.auth.dto.permission.PermissionResponse;
 import com.bintang.jwt.auth.repository.PermissionRepository;
 import com.bintang.jwt.auth.service.PermissionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +20,8 @@ public class PermissionController {
 
     private final PermissionService permissionService;
 
-
+    @PostMapping
+    public ResponseEntity<PermissionResponse> create(@RequestBody @Valid PermissionRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(permissionService.create(request));
+    }
 }
