dd3cb195e2566accd20ee60fd51d8ba2151af71b
bintangmada <bintang.mada.suharsono@bni.co.id>
2026-02-16 17:43:35 +0700

    feat: add refresh token

---
+++ src/main/java/com/bintang/jwt/auth/entity/RefreshToken.java
@@ -0,0 +1,29 @@
+package com.bintang.jwt.auth.entity;
+
+import com.bintang.jwt.auth.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_token")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    private Instant expiryDate;
+
+    private Long userId;
+
+}
