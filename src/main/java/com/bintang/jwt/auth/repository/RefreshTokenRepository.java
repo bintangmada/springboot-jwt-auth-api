ee0a23298cfaf2832da580e73c25141f84767ad2
bintangmada <bintang.mada.suharsono@bni.co.id>
2026-02-16 17:47:18 +0700

    feat: add refresh token repository

---
+++ src/main/java/com/bintang/jwt/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.bintang.jwt.auth.repository;
+
+import com.bintang.jwt.auth.entity.RefreshToken;
+import com.bintang.jwt.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUserId(Long userId);
+
+}
