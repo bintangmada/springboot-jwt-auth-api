f520a81c6a08f2cbc4f19bdfaa7befd303f23509
bintangmada <bintang.mada.suharsono@bni.co.id>
2026-02-16 17:39:02 +0700

    feat: add preauthorize to endpoint get all user

--- src/main/java/com/bintang/jwt/auth/controller/UserController.java
+++ src/main/java/com/bintang/jwt/auth/controller/UserController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +30,7 @@ public class UserController {
         return ResponseEntity.ok(userService.getById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN') and hasAuthority('USER_READ')")
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAll() {
         return ResponseEntity.ok(userService.getAll());
