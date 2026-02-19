de1ade7dc229c36cd97d153a4de556f488fa1c12
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-19 09:10:50 +0700

    feat: update role repository

--- src/main/java/com/bintang/jwt/auth/repository/RoleRepository.java
+++ src/main/java/com/bintang/jwt/auth/repository/RoleRepository.java
@@ -4,9 +4,17 @@ import com.bintang.jwt.auth.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
 
     boolean existsByName(String name);
 
     Role findByNameAndIsDeletedFalse(String name);
+
+    boolean existsByNameAndIsDeletedFalse(String name);
+
+    Optional<Role> findByIdAndIsDeletedFalse(Long id);
+
+    Optional<Role> findAllByIsDeletedFalse();
 }
